0912ab01a27fdb68cd8dd1d482a104a649d2f70d
sjMalik <surajit94malik@gmail.com>
2025-02-20 02:56:51 +0530

    user schema and seeds

--- database/migrations/20240915064923_create_users_table.js
+++ database/migrations/20240915064923_create_users_table.js
@@ -2,14 +2,26 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = async function (knex) {
+    await Promise.all([
+        knex.schema.createTable('users', (table) => {
+            table.increments('id').primary().unsigned(),
+                table.string('email').notNullable().unique(),
+                table.string('password').notNullable(),
+                table.string('first_name').notNullable(),
+                table.string('last_name').notNullable(),
+                table.enu('role', ['ADMIN', 'PURCHASING_MANAGER', 'SUPPLIER', 'SALES_MANAGER', 'CUSTOMER', 'OTHERS']),
+                table.timestamp('created_at').defaultTo(knex.fn.now());
+        }),
+    ]);
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = async function (knex) {
+    await Promise.all([
+        knex.raw('drop table if exists "users" cascade'),
+    ]);
 };
