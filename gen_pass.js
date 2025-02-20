7c778045e351cc2dac0a654e4fbf5c5a917291d9
sjMalik <surajit94malik@gmail.com>
2025-02-20 23:09:25 +0530

    gen pass script

---
+++ gen_pass.js
@@ -0,0 +1,15 @@
+const bcrypt = require("bcryptjs"),
+    crypto = require("crypto");
+
+const email = 'admin@gmail.com';
+const passwd = 'password';
+
+let salt = bcrypt.genSaltSync(10);
+
+let emailhash = crypto.createHash('sha256').update(email).digest('hex');
+let passwdhash = crypto.createHash('sha256').update(passwd).digest('hex');
+let tobehashed = [emailhash, passwdhash].join('+');
+
+let hash = bcrypt.hashSync(tobehashed, salt);
+
+console.log(hash);
